d8648ac17a96f987128beae65ab262322489a942
hitmachut <hitmachut@students.local>
2024-02-25 22:14:13 +0200

    first commit

---
+++ project2/HtmlHelper.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    public class HtmlHelper
+    {
+        public string[] AllTags { get; set; }
+        public string[] SelfClosingTags { get; set; }
+        private readonly static HtmlHelper _instance = new HtmlHelper();
+        public static HtmlHelper Instance => _instance;
+        public HtmlHelper()
+        {
+            try
+            {
+                var allTagsJson = File.ReadAllText("allTags.json");
+                var selfClosingTagsJson = File.ReadAllText("selfClosingTags.json");
+
+                AllTags = JsonSerializer.Deserialize<string[]>(allTagsJson);
+                SelfClosingTags = JsonSerializer.Deserialize<string[]>(selfClosingTagsJson);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading HTML tags: {ex.Message}");
+          
+            }
+
+
+        }
+       
+
+
+        public HtmlElement Build(List<string> htmlLines)
+        {
+
+            HtmlElement rootElement = null;
+            HtmlElement currentElement = null;
+
+            foreach (var line in htmlLines)
+            {
+                if (line == "html/")
+                {
+                    break;
+                }
+                if (line.StartsWith("/"))
+                {
+                    currentElement = currentElement?.Parent;
+                }
+                else if (HtmlHelper.Instance.AllTags != null && HtmlHelper.Instance.AllTags.Contains(line))
+                {
+
+                    var isSelfClosing = line.EndsWith("/") || HtmlHelper.Instance.SelfClosingTags.Contains(line);
+                    var newElement = new HtmlElement(line);
+                    var attributes = Regex.Matches(line, @"\w+\s*=\s*""([^""]*)""");
+                    foreach (Match attribute in attributes)
+                    {
+                        var attributeParts = attribute.Value.Split('=');
+                        var attributeName = attributeParts[0].Trim();
+                        var attributeValue = attributeParts[1].Trim('"');
+
+                        if (attributeName.ToLower() == "class")
+                        {
+
+                            var classes = attributeValue.Split(' ');
+                            foreach (var className in classes)
+                            {
+                                Console.WriteLine("class" + className);
+                                newElement.Classes.Add(className);
+                            }
+                        }
+                        else if (attributeName.ToLower() == "id")
+                        {
+
+                            newElement.Id = attributeValue;
+                        }
+                    }
+
+                    newElement.Parent = currentElement;
+
+                    if (currentElement == null)
+                    {
+                        rootElement = newElement;
+                    }
+                    else
+                    {
+                        currentElement.Children.Add(newElement);
+                    }
+
+                    if (!isSelfClosing)
+                    {
+                        currentElement = newElement;
+                    }
+                }
+                else
+                {
+                    if (currentElement != null)
+                        currentElement.InnerHtml = line;
+                }
+            }
+
+            return rootElement;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ project2/Program.cs
@@ -0,0 +1,77 @@
+﻿
+//var ele= "<div id=\"myId\" class=\"class1 class2\">text<div/>"; 
+//var attribute = new Regex("([^\\s]*?=\"(.*?)\"").Matches(ele);
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace project2
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var html = await Load("https://learn.malkabruk.co.il/practicode/projects/pract-2/#_3");
+            var cleanHtml = new Regex("\\s").Replace(html, "");
+            var htmlLines = new Regex("<(.*?)>").Split(cleanHtml).Where(s => s.Length > 0);
+            var arr = HtmlHelper.Instance.AllTags;
+            static async Task<string> Load(string url)
+            {
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(url);
+                var html = await response.Content.ReadAsStringAsync();
+                return html;
+            }
+            var root = new HtmlElement("div");
+            var child1 = new HtmlElement("div", root);
+            var child2 = new HtmlElement("p class=”class-name”", root);
+            var grandchild = new HtmlElement("p class=”class-name”", child1);
+            root.Children.Add(child1);
+            root.Children.Add(child2);
+            child1.Children.Add(grandchild);
+            Console.WriteLine("Descendants:");
+            foreach (var element in root.Descendants())
+            {
+                Console.WriteLine(element.Name);
+            }
+            Console.WriteLine("\nAncestors:");
+            foreach (var element in grandchild.Ancestors())
+            {
+                Console.WriteLine(element.Name);
+            }
+            string queryString = "div p.class-name";
+            Selector selector2 = Selector.ParseSelector(queryString);
+            Console.WriteLine("\nFindElements:");
+            var elements = root.FindElements(selector2);
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element.Name);
+                foreach (var child in element.Children)
+                {
+                    Console.WriteLine($"<{element.Name} class=\"{string.Join(" ", element.Classes)}\">{element.InnerHtml}</{element.Name}>");
+                }
+            }
+            HtmlHelper htmlHelper = new HtmlHelper();
+            List<string> values = new List<string>() { "/div", "div<", "/p", "class-name/>", "rf", "sd/", "ghgb" };
+            HtmlElement htmlElement2 = htmlHelper.Build(values);
+            var selector = Selector.ParseSelector("p");
+            var rootElement = HtmlHelper.Instance.Build(htmlLines.ToList());
+            root.FindElements(selector2);
+            Console.WriteLine(selector2.TagName);
+            Console.WriteLine(selector2.SelectorId);
+            Console.WriteLine(string.Join(", ", selector2.Classes));
+            Console.WriteLine(selector2.Parent?.TagName);
+
+        }
+
+    }
+}
+
+
+
